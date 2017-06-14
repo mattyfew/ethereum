057bbb44c2fed1752b54f08e89928f4a7c85d689
Matt Fewer <mattfewer@hotmail.com>
2017-06-14 13:04:59 +0200

    first commit

---
+++ eth-keypairs/keygen.js
@@ -0,0 +1,21 @@
+/*
+var Web3 = require('web3');
+var web3 = new Web3;
+
+console.log(web3.sha3("adfja;dsfja;sdfj"))
+*/
+
+var EthUtil = require('ethereumjs-util');
+
+var hexToBytes = function(hex) {
+    for(var bytes = [], c = 0; c < hex.length; c += 2){
+        bytes.push(parseInt(hex.substr(c, 2), 16));
+    }
+    return bytes
+}
+
+var privateKeyToAddress = function(privateKey) {
+    return `0x${EthUtil.privateToAddress(hexToBytes(privateKey)).toString('hex')}`
+}
+
+console.log(privateKeyToAddress(process.argv[2]));
---
+++ mainnet-transactions/script.js
@@ -0,0 +1,4 @@
+var endpoint = 'https://mainnet.infura.io/NADYHRplqOrAHFZhzRu3'
+var Web3 = require('web3')
+var EthTx = require('ethereum-tx')
+var web3 = new Web3(new Web3.providers.HttpProvider(endpoint))
