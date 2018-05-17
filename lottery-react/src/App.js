39ea623460f58f49ca83abe0a3c253a0e56df766
Matt Fewer <mattfewer@hotmail.com>
2018-05-17 09:49:38 +0200

    app completed but pickWinner is not working

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,23 +1,54 @@
-import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react'
 import web3 from './web3'
 import lottery from './lottery'
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { manager: '' }
+    state = {
+        manager: '',
+        players: [],
+        balance: '',
+        value: '',
+        message: ''
     }
+
     async componentDidMount() {
         // whenever we are working with the metamask provider, we do not need to provide a
         // 'from' property to the call() method, because it will by default use the
         // provider that is provided by metamask
-        console.log("about to call manager")
         const manager = await lottery.methods.manager().call()
-        console.log("we are here", manager)
+        const players = await lottery.methods.getPlayers().call()
+        const balance = await web3.eth.getBalance(lottery.options.address)
 
-        this.setState({ manager })
+        this.setState({ manager, players, balance })
+    }
+
+    onSubmit = async (event) => {
+        event.preventDefault()
+
+        const accounts = await web3.eth.getAccounts()
+
+        this.setState({ message: 'Waiting on transaction success...' })
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        })
+
+        this.setState({ message: 'You have been entered!' })
+    }
+
+    onClick = async () => {
+        const accounts = await web3.eth.getAccounts()
+
+        this.setState({ message: 'Waiting on transaction success...' })
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]
+        })
+
+        // extra credit: go back to the Lottery code and add a method to show who the winner is
+
+        this.setState({ message: 'A winner has been picked!' })
     }
 
     render() {
@@ -25,7 +56,34 @@ class App extends Component {
         return (
             <div>
                 <h2>Lotter Contract</h2>
-                <p>This contract is managed by { this.state.manager }</p>
+                <p>
+                    This contract is managed by { this.state.manager }.
+                    There are currently { this.state.players.length } people entered
+                    competing to win { web3.utils.fromWei(this.state.balance, 'ether') } ether!
+                </p>
+
+                <hr/>
+
+                <form onSubmit={ this.onSubmit }>
+                    <h4>Want to try your luck?</h4>
+                    <div>
+                        <label>Amout of ether to enter</label>
+                        <input
+                            value={ this.state.value }
+                            onChange={event => this.setState({ value: event.target.value })}
+                        />
+                    </div>
+                    <button>Enter</button>
+                </form>
+
+                <hr/>
+
+                <h4>Ready to pick a winner?</h4>
+                <button onClick={ this.onClick }>Pick a winner!</button>
+
+                <hr/>
+
+                <h1>{ this.state.message }</h1>
             </div>
         )
     }
